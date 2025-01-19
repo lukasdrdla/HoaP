b7324c448b7c7668f48a178fb4d575cc8695c991
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-19 18:22:43 +0100

    Add CustomerService for customer-related business logic.

---
+++ HoaP.Application/Services/CustomerService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HoaP.Application.Interfaces;
+using HoaP.Application.ViewModels.Customer;
+using HoaP.Domain.Entities;
+
+namespace HoaP.Application.Services
+{
+    public class CustomerService 
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<List<CustomerViewModel>> GetCustomers()
+        {
+            return await _customerRepository.GetCustomersAsync();
+        }
+
+        public async Task<UpdateCustomerViewModel> GetCustomerById(int id)
+        {
+            return await _customerRepository.GetCustomerByIdAsync(id);
+        }
+
+        public async Task CreateCustomer(CreateCustomerViewModel customer)
+        {
+            await _customerRepository.CreateCustomerAsync(customer);
+        }
+
+        public async Task UpdateCustomer(UpdateCustomerViewModel customer)
+        {
+            await _customerRepository.UpdateCustomerAsync(customer);
+        }
+
+        public async Task DeleteCustomer(int id)
+        {
+            await _customerRepository.DeleteCustomerAsync(id);
+        }
+    }
+}
