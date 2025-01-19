50f324ab19f1095f72a3ce67e68eef647e721e32
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-19 18:20:34 +0100

    Add AutoMapper profile for mapping between models and ViewModels.

---
+++ HoaP.Application/Mappings/CustomerProfile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HoaP.Application.ViewModels.Customer;
+using HoaP.Domain.Entities;
+
+namespace HoaP.Application.Mappings
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile() {
+
+            CreateMap<Customer, CustomerViewModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.PersonalIdentificationNumber, opt => opt.MapFrom(src => src.PersonalIdentificationNumber))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+
+            CreateMap<UpdateCustomerViewModel, Customer>();
+
+            CreateMap<Customer, UpdateCustomerViewModel>();
+
+            CreateMap<CreateCustomerViewModel, Customer>()
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+                .ForMember(dest => dest.PersonalIdentificationNumber, opt => opt.MapFrom(src => src.PersonalIdentificationNumber))
+                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.DocumentNumber, opt => opt.MapFrom(src => src.DocumentNumber))
+                .ForMember(dest => dest.PlaceOfBirth, opt => opt.MapFrom(src => src.PlaceOfBirth))
+                .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
+                .ForMember(dest => dest.DateOfIssue, opt => opt.MapFrom(src => src.DateOfIssue))
+                .ForMember(dest => dest.DateOfExpiry, opt => opt.MapFrom(src => src.DateOfExpiry))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.City))
+                .ForMember(dest => dest.PostalCode, opt => opt.MapFrom(src => src.PostalCode))
+                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country))
+                .ForMember(dest => dest.Nationality, opt => opt.MapFrom(src => src.Nationality))
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt))
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt));
+
+
+
+
+
+        }
+
+    }
+}
