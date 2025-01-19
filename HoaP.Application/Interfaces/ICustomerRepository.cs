f3f2615c7de685556982a586932e453e1d157da7
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-19 18:19:20 +0100

    Add ICustomerRepository interface.

---
+++ HoaP.Application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HoaP.Application.ViewModels.Customer;
+using HoaP.Domain.Entities;
+
+namespace HoaP.Application.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<List<CustomerViewModel>> GetCustomersAsync();
+        Task<UpdateCustomerViewModel> GetCustomerByIdAsync(int id);
+        Task CreateCustomerAsync(CreateCustomerViewModel customer);
+        Task UpdateCustomerAsync(UpdateCustomerViewModel customer);
+        Task DeleteCustomerAsync(int id);
+    }
+}
