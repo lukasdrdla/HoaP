350ae79a0b411d1d6bdcb2a8179699daadeb3024
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-20 21:12:17 +0100

    Create IRoomRepository interface.

---
+++ HoaP.Application/Interfaces/IRoomRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HoaP.Application.ViewModels.Room;
+using HoaP.Domain.Entities;
+
+namespace HoaP.Application.Interfaces
+{
+    public interface IRoomRepository
+    {
+        Task<List<RoomViewModel>> GetRooomsAsync();
+        Task<DetailRoomViewModel> GetRoomByIdAsync(int id);
+        Task CreateRoomAsync(CreateRoomViewModel room);
+        Task UpdateRoomAsync(UpdateRoomViewModel room);
+        Task DeleteRoomAsync(int id);
+    }
+}
