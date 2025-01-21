61a8c3f33f9e5f64eb8df107f691f46c7642f8b0
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-21 16:26:53 +0100

    Create DashBoardService.

---
+++ HoaP.Application/Services/DashBoardService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HoaP.Application.Interfaces;
+
+namespace HoaP.Application.Services
+{
+    public class DashBoardService
+    {
+        private readonly IDashBoardRepsoitory _dashBoardRepsoitory;
+
+        public DashBoardService(IDashBoardRepsoitory dashBoardRepsoitory)
+        {
+            _dashBoardRepsoitory = dashBoardRepsoitory;
+        }
+
+        public async Task<int> GetTotalCustomers()
+        {
+            return await _dashBoardRepsoitory.GetTotalCustomersAsync();
+        }
+
+        public async Task<int> GetTotalReservations()
+        {
+            return await _dashBoardRepsoitory.GetTotalBookingsAsync();
+        }
+
+        public async Task<int> GetTotalRooms()
+        {
+            return await _dashBoardRepsoitory.GetTotalRoomsAsync();
+        }
+
+        public async Task<decimal> GetRevenueFromLastMonth()
+        {
+            return await _dashBoardRepsoitory.GetRevenueFromLastMonthAsync();
+        }
+
+
+
+
+    }
+}
