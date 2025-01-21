eaa34c69b24cc40a6840b40cec2805871aacda1f
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-21 16:25:52 +0100

    Create DashBoardViewModel.

---
+++ HoaP.Application/ViewModels/DashBoard/DashBoardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoaP.Application.ViewModels.DashBoard
+{
+    public class DashBoardViewModel
+    {
+        public int TotalCustomers { get; set; }
+        public int TotalReservations { get; set; }
+        public int TotalRooms { get; set; }
+        public decimal RevenueFromLastMonth { get; set; }
+
+
+    }
+}
