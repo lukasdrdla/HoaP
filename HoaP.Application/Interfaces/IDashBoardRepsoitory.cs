af2147d754deb616d2cb6ea87b4853dcdb5aba37
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-21 16:25:00 +0100

    Create interface IDashBoardRepository.

---
+++ HoaP.Application/Interfaces/IDashBoardRepsoitory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoaP.Application.Interfaces
+{
+    public interface IDashBoardRepsoitory
+    {
+        Task<int> GetTotalCustomersAsync();
+        Task<int> GetTotalRoomsAsync();
+        Task<int> GetTotalBookingsAsync();
+        Task<decimal> GetRevenueFromLastMonthAsync();
+    }
+}
