5e4c8389253540b618986c88b28513538a4fdd5b
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-22 09:29:41 +0100

    Create InsuranceCompanyViewModel.

---
+++ HoaP.Application/ViewModels/InsuranceCompany/InsuranceCompanyViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoaP.Application.ViewModels.InsuranceCompany
+{
+    public class InsuranceCompanyViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Website { get; set; } = string.Empty;
+    }
+}
