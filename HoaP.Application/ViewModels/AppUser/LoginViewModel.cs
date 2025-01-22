65859059ddd2d25ee8742374dbb54a1b4955b05d
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-22 09:28:02 +0100

    Create LoginViewModel for logging to my application.

---
+++ HoaP.Application/ViewModels/AppUser/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoaP.Application.ViewModels.AppUser
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email je povinný")]
+        [EmailAddress(ErrorMessage = "Neplatný formát emailu")]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Heslo je povinné")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Heslo")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
