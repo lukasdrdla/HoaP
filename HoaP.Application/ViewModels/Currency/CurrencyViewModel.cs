40bd9905782931cc34fe0ebdb7f45beaa45be7a1
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-22 09:28:13 +0100

    Create CurrencyViewModel.

---
+++ HoaP.Application/ViewModels/Currency/CurrencyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoaP.Application.ViewModels.Currency
+{
+    public class CurrencyViewModel
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Symbol { get; set; } = string.Empty;
+    }
+}
