059334c12f0ee0a453dbf24be56af491fcbf9c02
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-23 10:49:03 +0100

    Edit mapping for invoice viewModel.

--- HoaP.Application/Mappings/InvoiceProfile.cs
+++ HoaP.Application/Mappings/InvoiceProfile.cs
@@ -33,12 +33,32 @@ namespace HoaP.Application.Mappings
                 .ForMember(dest => dest.Discount, opt => opt.MapFrom(src => src.Discount))
                 .ForMember(dest => dest.Prepayment, opt => opt.MapFrom(src => src.Prepayment))
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
-                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt));
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => src.UpdatedAt))
+                .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Reservation.Customer.FirstName + " " + src.Reservation.Customer.LastName));
 
 
-            CreateMap<CreateInvoiceViewModel, Invoice>();
 
-            CreateMap<UpdateInvoiceViewModel, Invoice>();
+            CreateMap<InvoiceFormViewModel, DetailInvoiceViewModel>()
+                .ForMember(dest => dest.CurrencyName, opt => opt.MapFrom(src => src.Currencies.Find(c => c.Id == src.CurrencyId).Name));
+            CreateMap<DetailInvoiceViewModel, InvoiceFormViewModel>()
+                .ForMember(dest => dest.CurrencyId, opt => opt.MapFrom(src => src.CurrencyId));
+
+            CreateMap<InvoiceFormViewModel, Invoice>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.ReservationId, opt => opt.MapFrom(src => src.ReservationId))
+                .ForMember(dest => dest.IssueDate, opt => opt.MapFrom(src => src.IssueDate))
+                .ForMember(dest => dest.DueDate, opt => opt.MapFrom(src => src.DueDate))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.Discount, opt => opt.MapFrom(src => src.Discount))
+                .ForMember(dest => dest.Prepayment, opt => opt.MapFrom(src => src.Prepayment))
+                .ForMember(dest => dest.CurrencyId, opt => opt.MapFrom(src => src.CurrencyId));
+
+
+
+
+
+
         }
     }
 }
