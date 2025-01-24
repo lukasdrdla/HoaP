78c2a7f320a01ba818b511482e798f7d09d23cc3
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-24 07:33:01 +0100

    Update AmenityViewModel for admin settings page.

--- HoaP.Application/ViewModels/Amenity/AmenityViewModel.cs
+++ HoaP.Application/ViewModels/Amenity/AmenityViewModel.cs
@@ -8,7 +8,10 @@ namespace HoaP.Application.ViewModels.Amenity
 {
     public class AmenityViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Icon { get; set; } = string.Empty;
+
+        public bool isSelected { get; set; }
     }
 }
