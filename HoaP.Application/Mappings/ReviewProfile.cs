7b16e87115b91bbeb440f163f7cdd0f95c3e645d
Lukáš Drdla <lukas.drdla01@gmail.com>
2025-01-28 10:44:02 +0100

    Add ReviewProfile for mapping.

---
+++ HoaP.Application/Mappings/ReviewProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HoaP.Application.ViewModels.Review;
+using HoaP.Domain.Entities;
+
+namespace HoaP.Application.Mappings
+{
+    public class ReviewProfile : Profile
+    {
+        public ReviewProfile()
+        {
+            
+            CreateMap<Review, ReviewViewModel>()
+                .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Customer.FirstName + " " + src.Customer.LastName))
+                .ForMember(dest => dest.RoomId, opt => opt.MapFrom(src => src.Room.Id))
+                .ReverseMap();
+
+        }
+    }
+}
